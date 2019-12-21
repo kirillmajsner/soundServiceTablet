99149ffaf4da8f4d4266411449d7a859bba5adbe
Kirill Maysner <kirillmajsner@gmail.com>
2019-12-21 14:35:52 +0600

    стилизация дорожки

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -33,3 +33,13 @@ $(function () {
 
 
 });
+$(function () {
+    var $ppc = $('.progress-pie-chart'),
+        percent = parseInt($ppc.data('percent')),
+        deg = 360 * percent / 100;
+    if (percent > 50) {
+        $ppc.addClass('gt-50');
+    }
+    $('.ppc-progress-fill').css('transform', 'rotate(' + deg + 'deg)');
+
+});
